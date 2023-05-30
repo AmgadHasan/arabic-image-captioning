684888e92db6e5b2cdfeb922ff18c5ac6d47fcc7
Amgad Hasan <109704569+AmgadHasan@users.noreply.github.com>
2023-05-30 17:58:59 +0300

    Update api.py

--- utils/api.py
+++ utils/api.py
@@ -6,11 +6,11 @@ from fastapi import FastAPI, Request, Form, File, UploadFile
 import uvicorn
 import os
 import json
-import tensorflow as tf
-from tf.keras.saving import load_model
-from tf.keras.applications.inception_v3 import preprocess_input
+# import tensorflow as tf
+# from tf.keras.saving import load_model
+# from tf.keras.applications.inception_v3 import preprocess_input
 from pydantic import BaseModel
-from customfile import ImageLoader, ImageCaptioner, load_tokenizer    # To be modified
+from dummy_model import ImageLoader, ImageCaptioner#, load_tokenizer    # To be modified
 from fastapi.templating import Jinja2Templates
 
 # Declaring our FastAPI instance
@@ -18,23 +18,23 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 # Setting paths and directories to models
-CWD = os.getcwd()
-PWD = os.path.dirname(CWD)
-MODELS_FOLDER_PATH = PWD + "/models/"
-CNN_PATH = models_folder_path + 'cnn'
-CNN_ENCODER_PATH = MODELS_FOLDER_PATH + 'cnn_encoder'
-RNN_DECODER_PATH = MODELS_FOLDER_PATH + 'rnn_decoder'
-TOKENIZER_PATH = MODELS_FOLDER_PATH + 'tokenizer.json'
+#CWD = os.getcwd()
+#PWD = os.path.dirname(CWD)
+#MODELS_FOLDER_PATH = PWD + "/models/"
+#CNN_PATH = models_folder_path + 'cnn'
+#CNN_ENCODER_PATH = MODELS_FOLDER_PATH + 'cnn_encoder'
+#RNN_DECODER_PATH = MODELS_FOLDER_PATH + 'rnn_decoder'
+#TOKENIZER_PATH = MODELS_FOLDER_PATH + 'tokenizer.json'
 
 # Loading models and tokenizer
-cnn = load_model(CNN_PATH)
-cnn_encoder = load_model(CNN_ENCODER_PATH)
-rnn_decoer = load_model(RNN_DECODER_PATH)
-tokenizer = load_tokenizer(TOKENIZER_PATH)
-
-
+#cnn = load_model(CNN_PATH)
+#cnn_encoder = load_model(CNN_ENCODER_PATH)
+#rnn_decoer = load_model(RNN_DECODER_PATH)
+#tokenizer = load_tokenizer(TOKENIZER_PATH)
+preprocess_input = 35
+dummy = 2
 # Creating an image caption generator model from submodels
-image_captioner = ImageCaptioner(cnn, cnn_encoder, rnn_decoer)
+image_captioner = ImageCaptioner(dummy)
 image_loader = ImageLoader(preprocessor = preprocess_input)
 
 @app.get("/")
@@ -43,10 +43,13 @@ async def home(request: Request):
 
 @app.post("/predict")
 async def predict(request: Request, file: UploadFile = File(...)):
-    image_path = await file.read()
+    contents = await file.read()
     # Use the contents of the uploadedfile to generate text for the image
-    image = image_loader.load_image(image_path)
-
-    tokens, attention_weights = custom_model.predict(image)
-    caption = tokenizer.sequences_to_texts([tokens])
-    return templates.TemplateResponse("index.html", {"request": request, "caption": caption})
+    
+    image = image_loader.load_image(contents)
+    tokens, _ = image_captioner.predict(image)
+    words = ["HI", 'hello', 'hey', 'hesy', 'asfasf','asfasfasf','asfaasfasf','..','...', 'qfqfqf','adfafd']
+    caption = [words[i] for i in tokens]
+    #tokens, attention_weights = custom_model.predict(image)
+    #caption = tokenizer.sequences_to_texts([tokens])
+    return templates.TemplateResponse("index.html", {"request": request, "prediction": caption})
